05075ae869455883ec5bd2b822a00bd821e394a9
tobizen <48719850+tobizen@users.noreply.github.com>
2019-05-03 10:44:04 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		calc.setDefaultCloseOperation(calc.EXIT_ON_CLOSE);
+		calc.setVisible(true);
+		calc.setSize(600, 500);
+	}
+
+}
